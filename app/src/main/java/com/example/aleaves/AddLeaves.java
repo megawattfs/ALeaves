2b74c55f97781c5745cbc2031f5a899c28577dcd
Margaret Schrayer <margaretschrayer@gmail.com>
2020-02-11 11:53:05 -0500

    added activities

---
+++ app/src/main/java/com/example/aleaves/AddLeaves.java
@@ -0,0 +1,14 @@
+package com.example.aleaves;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddLeaves extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_leaves);
+    }
+}

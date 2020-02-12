b3ddf55549fec8555a24362ea343820d32ba73f7
Margaret Schrayer <margaretschrayer@gmail.com>
2020-02-12 13:07:18 -0500

    started implementing camera2 api

--- app/src/main/java/com/example/aleaves/AddLeaves.java
+++ app/src/main/java/com/example/aleaves/AddLeaves.java
@@ -1,6 +1,9 @@
 package com.example.aleaves;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.hardware.camera2.*;
+import android.content.Context;
+import android.content.pm.PackageManager;
 
 import android.os.Bundle;
 
@@ -10,5 +13,39 @@ public class AddLeaves extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_leaves);
+
+        //Check if the device has a camera available
+        boolean hasCamera = checkCameraHardware(getApplicationContext());//Check if getApplicationContext() call correct
     }
+
+    //TODO: Request access to camera and/or photos
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    //TODO is this using up-to-date cam?
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+
+
+
+
 }
+
---
+++ app/src/main/java/com/example/aleaves/LeafCam.java
@@ -0,0 +1,8 @@
+package com.example.aleaves;
+
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+public class LeafCam extends SurfaceView implements SurfaceHolder {
+
+}
